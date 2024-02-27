95c9fea5be27891d4448f6caae02b31120b6bcd8
FOFIE2002 <fofiefopaelisabeth@gmail.com>
2024-02-27 16:55:21 +0100

    image sections and styles

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ var swiper = new Swiper(".home", {
       delay: 2000,
       disableOnInteraction: false,
     },
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
